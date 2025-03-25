d92d1cb212e7fcb2efa586464a0205c1d749367c
sylsylviane <syl.sylviane1@gmail.com>
2025-03-25 17:21:29 -0400

    Créer le controlleur pour l'inscription des utilisateurs

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => ['required', 'confirmed', Password::min(2)->max(20)->letters()->mixedCase()->numbers()],
+        ]);
+
+        $user = new User();
+        $user->fill($request->all());
+        $user->save();
+
+        // 
+        return redirect(route('accueil'))->with('success', __('utilisateur_ajoute_succes'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
