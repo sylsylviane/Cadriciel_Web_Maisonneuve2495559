8eb1499c1755d875b96be39864d62208f90f54fb
sylsylviane <syl.sylviane1@gmail.com>
2025-03-25 13:17:14 -0400

    Ajouter la traduction dans la page d'accueillll

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', 'Accueil')
+@section('title', trans('Accueil'))
 @section('content')
 
 <!-- Page d'accueil-->
@@ -9,10 +9,10 @@
             <img src="{{asset('img/etudiants.jpg')}}" class="d-block mx-lg-auto img-fluid w-100" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
         </div>
         <div class="col-lg-6">
-            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Bienvenue sur l'application du Collège Maisonneuve !</h1>
-            <p class="lead">Nous avons créé cette plateforme pour que les étudiants du Collège Maisonneuve puissent fournir leurs informations personnelles de manière rapide, sécurisée et efficace. Vos données sont essentielles pour améliorer nos services et garantir une meilleure communication avec notre communauté étudiante.</p>
+            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">@lang('lang.bienvenue_titre')"</h1>
+            <p class="lead">@lang('lang.accueil_paragraphe')</p>
             <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                <a href="{{route('students.index')}}" class="btn p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 ">Voir les étudiants</a>
+                <a href="{{route('students.index')}}" class="btn p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 ">@lang('lang.texte_bouton')</a>
             </div>
         </div>
     </div>
