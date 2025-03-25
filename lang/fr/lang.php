2912b7bcf814c331303f50471ea6df22e6330240
sylsylviane <syl.sylviane1@gmail.com>
2025-03-25 13:14:44 -0400

    Ajouter fichier pour la traduction

---
+++ lang/fr/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "bienvenue_titre" => "Bienvenue sur l'application du Collège Maisonneuve !",
+    "accueil_paragraphe" => "<p class='lead'>Nous avons créé cette plateforme pour que les étudiants du Collège Maisonneuve puissent fournir leurs informations personnelles de manière rapide, sécurisée et efficace. Vos données sont essentielles pour améliorer nos services et garantir une meilleure communication avec notre communauté étudiante.</p>",
+    "texte_bouton" => "Voir les étudiants",
+    "texte_pied_de_page" => "Tous droits réservés",
+];
