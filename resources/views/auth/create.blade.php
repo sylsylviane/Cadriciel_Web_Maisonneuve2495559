7b77c890bd0b09ede224c1b7fe8768338dc8655c
sylsylviane <syl.sylviane1@gmail.com>
2025-03-29 10:38:49 -0400

    Ajouter les erreurs de validation au formulaire de login

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.app')
 @section('title', trans('Login'))
 @section('content')
+
+<!-- AFFICHAGE DES ERREURS DE VALIDATION -->
+@if(!$errors->isEmpty())
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+    </ul>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
+<!-- FIN AFFICHAGE DES ERREURS DE VALIDATION -->
+
+<!-- FORMULAIRE DE LOGIN -->
 <div class="form-signin w-100 m-auto mt-5" style="max-width: 330px;">
-    <!-- TODO: AFFICHER LES ERREURS DE VALIDATION, LES OLD VALUE ET NAME -->
     <form method="POST">
         @csrf
         <h1 class="h3 mb-3 fw-normal">@lang('Connexion')</h1>
         <div class="form-floating">
-            <input type="email" class="form-control rounded-bottom-0" id="floatingInput" placeholder="name@example.com">
+            <input name="email" type="email" class="form-control rounded-bottom-0" id="floatingInput" placeholder="name@example.com" value="{{old('email')}}">
             <label for="floatingInput">@lang('Courriel')</label>
         </div>
         <div class="form-floating">
-            <input type="password" class="form-control rounded-top-0" id="floatingPassword" placeholder="Password">
+            <input name="password" type="password" class="form-control rounded-top-0" id="floatingPassword" placeholder="Password" value="{{old('password')}}">
             <label for="floatingPassword">@lang('Mot de passe')</label>
         </div>
 
