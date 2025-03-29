70ba1fc2832634f24c90de5e7a64858fdb100aca
sylsylviane <syl.sylviane1@gmail.com>
2025-03-29 10:41:09 -0400

    Ajouter un nouveau message de succès pour le login

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -16,5 +16,5 @@ return [
     'failed' => 'Ces informations d\'identification ne correspondent pas à nos enregistrements.',
     'password' => 'Le mot de passe fourni est incorrect.',
     'throttle' => 'Trop de tentatives de connexion. Veuillez essayer de nouveau dans :seconds secondes.',
-
+    'loginSuccess' => 'Vous êtes connecté',
 ];
