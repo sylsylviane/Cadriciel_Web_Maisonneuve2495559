9fdc40e8d114b793ae98b98912fb72041e4f0de9
sylsylviane <syl.sylviane1@gmail.com>
2025-03-29 10:40:04 -0400

    Modifier le texte du bouton de la section hero dans l'accueil

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,7 +3,7 @@
 return [
     "bienvenue_titre" => "Bienvenue sur l'application du Collège Maisonneuve !",
     "accueil_paragraphe" => "<p class='lead'>Nous avons créé cette plateforme pour que les étudiants du Collège Maisonneuve puissent fournir leurs informations personnelles de manière rapide, sécurisée et efficace. Vos données sont essentielles pour améliorer nos services et garantir une meilleure communication avec notre communauté étudiante.</p>",
-    "texte_bouton" => "Voir les étudiants",
+    "consulter_forum" => "Consulter le forum",
     "texte_pied_de_page" => "Tous droits réservés",
     "texte_modale_suppression" => "<h5 class='mb-0'>Êtes-vous sûr de vouloir supprimer cet étudiant ?</h5><p class='mb-0'>Cette action est irréversible.</p>"
 ];
