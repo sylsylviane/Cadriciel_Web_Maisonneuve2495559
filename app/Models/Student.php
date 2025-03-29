84c7006f9e5aa8bd7ddc168e3268841aa76179ed
sylsylviane <syl.sylviane1@gmail.com>
2025-03-29 10:59:01 -0400

    Ajouter une relation en l'utilisateur et l'étudiant créé

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -15,7 +15,8 @@ class Student extends Model
         'phone',
         'email',
         'dob',
-        'city_id'
+        'city_id',
+        'user_id'
     ];
 
     /**
@@ -23,6 +24,11 @@ class Student extends Model
      */
     public function city(){
         return $this->belongsTo(City::class);
+    }
+
+    // Permet de définir et d'accéder à l'utilisateur qui a créé l'étudiant
+    public function user(){
+        return $this->belongsTo(User::class);
     }
 
     // Mutateur pour le nom de l'étudiant : convertit la première lettre de chaque mot en majuscule et le reste en minuscule avant de l'enregistrer dans la base de données et prend en compte les caractères accentués (UTF-8)
