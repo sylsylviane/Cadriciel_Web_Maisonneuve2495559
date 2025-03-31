4b84e706cb66f3db10fda2a39f54c46a49ecb853
sylsylviane <syl.sylviane1@gmail.com>
2025-03-31 17:43:36 -0400

    Modifier fichiers pour la traduction

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,7 +4,7 @@ return [
     "text_accueil" => "Home",
     "bienvenue_titre" => "Welcome to the College Maisonneuve app!",
     "accueil_paragraphe" => "<p class='lead'>We created this platform so that College Maisonneuve students can provide their personal information quickly, securely, and efficiently. Your data is essential to improving our services and ensuring better communication with our student community.</p>",
-    "texte_bouton" => "View students",
+    "consulter_forum" => "Consult the forum",
     "texte_pied_de_page" => "All rights reserved",
     "texte_modale_suppression" => "<h5 class='mb-0'>Are you sure you want to delete this student ?</h5><p class='mb-0'>This action is irreversible.</p>"
 ];
