6bba89f50ab95209a2513c3f90df57cba51ddeaf
sylsylviane <syl.sylviane1@gmail.com>
2025-04-09 15:51:38 -0400

    Créer les fichiers de migration pour la table fichiers

---
+++ database/migrations/2025_04_04_132142_create_fichiers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fichiers', function (Blueprint $table) {
+            $table->id();
+            $table->json('title');
+            $table->unsignedBigInteger('user_id'); // unsignedBigInteger est utilisé pour les clés étrangères
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fichiers');
+    }
+};
